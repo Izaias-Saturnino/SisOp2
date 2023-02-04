52f4ee519270a47c6be262ff3d8f7363d87506ee
Izaias Neto <izaiassaturninodelimaneto@hotmail.com>
2023-02-04 18:57:58 -0300

    download fix

--- server/server.cpp
+++ server/server.cpp
@@ -162,7 +162,7 @@ void *ThreadClient(void *arg)
                 for (int i =0 ;i<fragments.size();i++){
                     for(int j=0;j<fragments.at(i).size();j++){
                         char* frag = &(fragments.at(i).at(j));
-                        printf("%x ",(unsigned char)fragments.at(i).at(j));
+                        //printf("%x ",(unsigned char)fragments.at(i).at(j));
                         file_server.write(frag, sizeof(char));
                     }
                 }
@@ -258,12 +258,13 @@ int upload_file_server(int sock, char username[], std::string file_path)
 		file.seekg(0);
 
 		sendMessage((char *)file_path.c_str(), 1, MENSAGEM_ENVIO_NOME_ARQUIVO, std::ceil(file_size / 256), username, sock);
+        sleep(1);
 		for (int i = 0; i < file_size; i += ((sizeof(buffer)))) // to read file
 		{
 			memset(buffer, 0, 256);
 			file.read(buffer, sizeof(buffer));
             for(int i =0;i<256; i++){
-                printf("%x ", (unsigned char)buffer[i]);
+                //printf("%x ", (unsigned char)buffer[i]);
             }
             
 			sendMessage(buffer, i / 256, MENSAGEM_ENVIO_PARTE_ARQUIVO, 4, username, sock);
