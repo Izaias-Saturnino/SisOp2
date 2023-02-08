5ec5bcf7a41643f35e2cab01a620238f4246b27c
Izaias Neto <izaiassaturninodelimaneto@hotmail.com>
2023-02-08 10:01:04 -0300

    login count fix

--- login/login.cpp
+++ login/login.cpp
@@ -37,9 +37,9 @@ void LoginManager::criarNovoUsuario(char nome[],int socketCli){
 void LoginManager::Logout(char user[],int socket, char resposta[]){
     vector<USUARIO>::iterator it;
     
+    mtx_sessoes.lock(); 
     for(it = this->listaDeUsuarios.begin(); it != this->listaDeUsuarios.end(); it++){
-        if(strcmp(user,(*it).nome) == 0 ){  
-            mtx_sessoes.lock();       
+        if(strcmp(user,(*it).nome) == 0){
             if((*it).socketClient1 == socket)
             {
                 (*it).sessaoAtiva1 = false;
@@ -51,10 +51,11 @@ void LoginManager::Logout(char user[],int socket, char resposta[]){
                 (*it).sync2 = -1;
                 strcpy(resposta,"Sessao 2 desconectada");
             }
-            mtx_sessoes.unlock();
             break;
         }
     }
+    mtx_sessoes.unlock();
+
     if((*it).sessaoAtiva1 == false && ((*it).sessaoAtiva2 == false)) //sem conexão -> remove da lista
     {
         strcpy(resposta,"Usuario desconectado");
@@ -62,54 +63,39 @@ void LoginManager::Logout(char user[],int socket, char resposta[]){
     
 }
 
-bool LoginManager::verificaQuantidadeUsuarios(char nome[],int socketCli){
+bool LoginManager::login(int socketCli, char nome[]){
     vector<USUARIO>::iterator it;
     bool achou = false, usuarioValido = true;
 
+    mtx_sessoes.lock();
     for(it = this->listaDeUsuarios.begin(); it != this->listaDeUsuarios.end(); it++){
         if(strcmp(nome,(*it).nome) == 0 ){
             achou = true;
-            mtx_sessoes.lock();
-            if((*it).sessaoAtiva1 == true)
+            if((*it).sessaoAtiva1 == false)
             {
-                if((*it).sessaoAtiva2 == true){
-                    cout<<"Excedido o número de sessoes possiveis \n"<<endl;
-                    usuarioValido = false;
-                }
-                else{
-                    (*it).sessaoAtiva2 = true;
-                    (*it).socketClient2 = socketCli;
-                    cout<<"Segunda conta ativada \n"<<(*it).socketClient2<<endl;
-                }
+                (*it).sessaoAtiva1 = true;
+                (*it).socketClient1 = socketCli;
+                cout << "Conta da pos 1 ativada. socket: " << socketCli << endl;
+            }
+            else if((*it).sessaoAtiva2 == false){
+                (*it).sessaoAtiva2 = true;
+                (*it).socketClient2 = socketCli;
+                cout << "Conta da pos 2 ativada. socket: " << socketCli << endl;
             }
             else{
-                 if((*it).sessaoAtiva2 == true){ //reativa sessão 1
-                    (*it).sessaoAtiva1 = true;
-                    (*it).socketClient1 = socketCli;
-                    cout<<"Segunda conta ativada \n"<<(*it).socketClient1<<endl;
-                 }
-                 else{
-                    (*it).sessaoAtiva1 = true;
-                    cout<<"Primeira conta ativada \n"<<endl;
-                 }
+                cout << "Excedido o número de sessoes possiveis\n" << endl;
+                usuarioValido = false;
             }
-            mtx_sessoes.unlock();
+            break;
         }
 	}
+    mtx_sessoes.unlock();
 
     if(achou != true){
         cout<<"Usuário não encontrado!"<<endl<< "Criando um novo usuario...\n"<<endl;
         this->criarNovoUsuario(nome,socketCli);
     }
 
-    return usuarioValido;
-}
-
-bool LoginManager::login(int socketCli, char user[]){
-    bool usuarioValido;
-
-    usuarioValido = this->verificaQuantidadeUsuarios(user,socketCli);
-    
     return usuarioValido;
 }
 
