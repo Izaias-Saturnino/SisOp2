1e30b69908f7fc9aa36179e18fc1b9d4ed8eebc9
Izaias Neto <izaiassaturninodelimaneto@hotmail.com>
2023-02-09 10:54:59 -0300

    quick fix

--- common/common.cpp
+++ common/common.cpp
@@ -15,16 +15,7 @@ int readSocket(PACKET *pkt, int sock){
     	/* read from the socket */
 		int result = read(sock, pkt+n, sizeof(*pkt)-n);
 
-		if (result == 0){
-			cout << "no connection" << endl;
-			break;
-		}
-		if (result < 0){
-			cout << "connection ERROR" << endl;
-			break;
-		}
-
-		if (result > 0) 
+		if (result >= 0) 
 		{
 			n += result;
 		}
